4bde35ff76c11f0196668a15da316f116b09596e
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2024-09-25 16:08:15 +0700

    url file

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
             </Routes>
-            <Logout setLoggedIn={setLoggedIn} />
+            <Logout />
           </div>
         )}
       </div>
