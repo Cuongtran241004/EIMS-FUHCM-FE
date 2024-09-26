087ee07702e45ded1c88710b88bb60d56f8e18ba
Tran Van Duy <duytvse183134@fpt.edu.vn>
2024-09-26 11:26:31 +0700

    fix login

---
+++ FE/src/pages/Login/Form.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Button, Checkbox, Form as form, Input } from 'antd';
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+function Form() {
+    return (
+
+        <form
+    name="basic"
+    labelCol={{
+      span: 8,
+    }}
+    wrapperCol={{
+      span: 16,
+    }}
+    style={{
+      maxWidth: 600,
+      fontWeight: 600,
+    }}
+    initialValues={{
+      remember: true,
+    }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <form.Item
+      label="Email"
+      name="Email"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your username!',
+        },
+      ]}
+    >
+      <Input />
+    </form.Item>
+
+    <form.Item
+      label="Password"
+      name="password"
+      rules={[
+        {
+          required: true,
+          message: 'Please input your password!',
+        },
+      ]}
+    >
+      <Input.Password />
+    </form.Item>
+
+   
+
+    <form.Item
+      wrapperCol={{
+        offset: 8,
+        span: 16,
+      }}
+    >
+      <Button type="primary" htmlType="submit">
+        Login
+      </Button>
+    </form.Item>
+  </form>
+    );
+  
+};
+
+export default Form;
