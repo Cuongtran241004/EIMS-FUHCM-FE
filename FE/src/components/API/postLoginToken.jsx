a789a32aa91cf992d9b5d74d3933411f48c12740
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2024-10-02 23:53:12 +0700

    Update postLogintoken

--- FE/src/components/API/postLoginToken.jsx
+++ FE/src/components/API/postLoginToken.jsx
@@ -21,30 +21,29 @@
 //     }
 //   };
 
-
-import axios from 'axios';
+import axios from "axios";
 
 export const postLoginToken = async (idToken) => {
-    const API_URL = import.meta.env.VITE_APP_API_URL;
-    const path = '/v1/oauth/login';
+  const API_URL = import.meta.env.VITE_APP_API_URL;
+  const path = "/v1/oauth/login";
 
-    try {
-        const response = await axios.post(
-            `${API_URL}${path}`,
-            JSON.stringify(idToken), // Sending the token as the request body
-            {
-                withCredentials: true, // Include credentials like cookies in the request
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-            }
-        );
+  try {
+    const response = await axios.post(
+      `${API_URL}${path}`,
+      JSON.stringify(idToken), // Sending the token as the request body
+      {
+        withCredentials: true, // Include credentials like cookies in the request
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-        // If the request is successful, Axios will return a response object
-        return response.status === 200; // Check for success by the status code
-    } catch (e) {
-        console.error('postLoginToken Error: ', e.message);
-        return false;
-    }
+    // If the request is successful, Axios will return a response object
+    return response.status === 200; // Check for success by the status code
+  } catch (e) {
+    console.error("postLoginToken Error: ", e.message);
+    return false;
+  }
 };
