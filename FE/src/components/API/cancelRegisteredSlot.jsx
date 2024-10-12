fe76eb8f9c12c3ae5ebbad884cc7217ed0d4552a
Tran Van Duy <duytvse183134@fpt.edu.vn>
2024-10-13 00:07:07 +0700

    Update Select

---
+++ FE/src/components/API/cancelRegisteredSlot.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const cancelRegisteredSlot = async (slotId) => {
+    const API_URL = import.meta.env.VITE_APP_API_URL;
+    const path = '';
+    console.log(slotId);
+    try {
+      const response = await axios.post(`${API_URL}${path}`, slotId, {
+        withCredentials: true,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      return response.status === 201;
+    } catch (e) {
+      throw new Error(e.response?.data?.message || 'Error registering for slots.');
+    }
+};
--- FE/src/pages/Invigilator/InvigilatorRegistration.jsx
+++ FE/src/pages/Invigilator/InvigilatorRegistration.jsx
@@ -1,17 +1,16 @@
 import { UserContext } from '../../components/UserContext';
 import React, { useState, useEffect, useContext } from 'react';
-import { Dropdown, message, Button, Space } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import { Dropdown, message, Button, Space, Modal, Checkbox } from 'antd';
+import { DownOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import { postRegisterSlots } from '../../components/API/postRegisterSlots';
-import { getSemester } from '../../components/API/getSemester';
-import { availableSlots } from '../../components/API/availableSlots';
-import { schedules } from '../../components/API/schedules';
+import { cancelRegisteredSlot } from '../../components/API/cancelRegisteredSlot';
 import { useSemester } from '../../components/SemesterContext';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './invigilatorRegistration.css'
 
+const { confirm } = Modal;
 const localizer = momentLocalizer(moment);
 
 function InvigilatorRegistration() {
@@ -19,9 +18,11 @@ function InvigilatorRegistration() {
   const { semesters = [], selectedSemester, setSelectedSemester, examSlotDetail, availableSlotsData } = useSemester();
   const [events, setEvents] = useState([]);
   const [selectedSlots, setSelectedSlots] = useState([]);
+  const [cancelModalVisible, setCancelModalVisible] = useState(false);
+  const [selectedCancelSlots, setSelectedCancelSlots] = useState([]);
+  const [isAllSelected, setIsAllSelected] = useState(false);
+
   const allowedSlots = selectedSemester?.allowedSlotConfig;
-
-
 
   useEffect(() => {
     if (selectedSemester) {
@@ -52,8 +53,6 @@ function InvigilatorRegistration() {
       return;
     }
 
-
-
     if (selectedSlots.includes(examSlotId)) {
       setSelectedSlots(selectedSlots.filter((slotId) => slotId !== examSlotId));
     } else {
@@ -64,7 +63,6 @@ function InvigilatorRegistration() {
       }
     }
   };
-  console.log('selectedSlots:', selectedSlots);
 
   const handleRegister = async () => {
     if (!selectedSemester) {
@@ -82,9 +80,6 @@ function InvigilatorRegistration() {
     const registeredSlots = flatEvents.filter((slot) =>
       selectedSlots.includes(slot.examSlotId)
     );
-
-
-
 
     const slotIds = registeredSlots.map((slot) => slot.examSlotId);
     const slots = {
@@ -107,21 +102,70 @@ function InvigilatorRegistration() {
   };
 
 
+  const showCancelModal = () => {
+    setCancelModalVisible(true);
+  };
+
+  const handleCancelModalClose = () => {
+    setSelectedCancelSlots([]);
+    setCancelModalVisible(false);
+  };
+
+
+  const handleCancelSlotChange = (examSlotId, checked) => {
+    if (checked) {
+      setSelectedCancelSlots([...selectedCancelSlots, examSlotId]);
+    } else {
+      setSelectedCancelSlots(selectedCancelSlots.filter(id => id !== examSlotId));
+    }
+  };
+
+  const handleSelectAll = (checked) => {
+    if (checked) {
+      const allSlotIds = examSlotDetail.map(slot => slot.examSlotId);
+      setSelectedCancelSlots(allSlotIds);
+    } else {
+      setSelectedCancelSlots([]);
+    }
+    setIsAllSelected(checked);
+  };
+
+
+  const handleConfirmCancel = async () => {
+    confirm({
+      title: 'Do you want to cancel these slots?',
+      icon: <ExclamationCircleOutlined />,
+      content: 'This action cannot be undone.',
+      onOk: async () => {
+        try {
+          const promises = selectedCancelSlots.map((slotId) => cancelRegisteredSlot(slotId));
+          await Promise.all(promises);
+          message.success('Slot(s) cancelled successfully');
+          setSelectedCancelSlots([]);
+          setCancelModalVisible(false);
+        } catch (error) {
+          message.error('Error cancelling slot(s)');
+          console.error(error);
+        }
+      },
+
+    });
+  };
+
   const EventComponent = ({ event }) => (
     <div>
-      <p style={{margin: 0, fontWeight: 500, fontSize: 13.33333}}>{new Date(event.startAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}-
-      {new Date(event.endAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
-      
+      <p style={{ margin: 0, fontWeight: 500, fontSize: 13.33333 }}>
+        {new Date(event.startAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}-
+        {new Date(event.endAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+      </p>
     </div>
-
-
   );
-  console.log('Event:', events);
+
   return (
     <div>
       <h2 style={{ marginTop: 10, marginBottom: 0, marginLeft: 50 }}>Invigilator Register</h2>
       <div style={{ display: 'flex', alignItems: 'flex-start' }}>
-        <Calendar 
+        <Calendar
           localizer={localizer}
           events={availableSlotsData}
           startAccessor={(event) => new Date(event.startAt)}
@@ -157,10 +201,9 @@ function InvigilatorRegistration() {
               },
             };
           }}
-
         />
         <div style={{ marginTop: 40 }}>
-          <Dropdown menu={menu} trigger={['click']} >
+          <Dropdown menu={menu} trigger={['click']}>
             <Button size="large" style={{ width: '100%' }}>
               <Space>
                 {selectedSemester ? selectedSemester.name : 'No Semesters Available'}
@@ -176,11 +219,54 @@ function InvigilatorRegistration() {
           >
             Register
           </Button>
-          <Button>Cancel</Button>
+          <Button onClick={showCancelModal} style={{ marginTop: 10, width: '100%', height: 40 }}>
+            Cancel Slots
+          </Button>
           <p>Registered Slots: {examSlotDetail.length} / {allowedSlots}</p>
           <p>Selected Slots: {selectedSlots.length}</p>
         </div>
       </div>
+
+
+      <Modal
+        title="Select Slots to Cancel"
+        open={cancelModalVisible}
+        onOk={handleConfirmCancel}
+        onCancel={handleCancelModalClose}
+        okText="Confirm"
+        cancelText="Close"
+      >
+        <div style={{display: 'flex', marginBottom: 10, justifyContent: 'space-between' }}>
+          <p>Select All</p>
+          <Checkbox
+            onChange={(e) => handleSelectAll(e.target.checked)}
+            checked={isAllSelected}
+          >
+          </Checkbox>
+        </div>
+        {examSlotDetail.length === 0 ? (
+          <p>No registered slots available to cancel.</p>
+        ) : (
+          <div>
+            {examSlotDetail.map(slot => (
+              <div
+                key={slot.examSlotId}
+                style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '8px', width: '100%' }}
+              >
+                <span >
+                  {new Date(slot.startAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} - {new Date(slot.endAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })},
+                  {new Date(slot.startAt).toLocaleDateString()}
+                </span>
+
+                <Checkbox
+                  checked={selectedCancelSlots.includes(slot.examSlotId)}
+                  onChange={(e) => handleCancelSlotChange(slot.examSlotId, e.target.checked)}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </Modal>
     </div>
   );
 }
