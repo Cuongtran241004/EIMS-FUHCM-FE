8bdf43f278057732f97b65e0305f8cf5f1939eaa
Tran Quoc Cuong <cuongtqse182989@fpt.edu.vn>
2024-10-13 20:17:47 +0700

    Update profile page

--- FE/src/pages/Manager/Dashboard.jsx
+++ FE/src/pages/Manager/Dashboard.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import NavBar_Manager from "../../components/NavBar/NavBar_Manager";
 import Header from "../../components/Header/Header.jsx";
-import { Layout } from "antd";
+import { Layout, Typography, Row, Col, Card, Statistic } from "antd";
+import { Bar } from "react-chartjs-2"; // Make sure you have installed react-chartjs-2
+import { useEffect, useState } from "react";
 
-const { Sider } = Layout;
+const { Content, Sider } = Layout;
+const { Title } = Typography;
+
 const Dashboard = () => {
   return (
     <Layout style={{ height: "100vh" }}>
@@ -16,4 +20,5 @@ const Dashboard = () => {
     </Layout>
   );
 };
+
 export default Dashboard;
